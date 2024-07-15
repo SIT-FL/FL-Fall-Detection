bc093a02293c9d08bcdc38afa39349e7e4fd1863
P <30462819+ptrpfa@users.noreply.github.com>
2024-07-15 19:55:24 +0800

    added expanded datasets

--- config.py
+++ config.py
@@ -1,5 +1,14 @@
 """ Configurations """
+# Parameters
+RAND_SEED = 123456
+SPLIT_SIZE = 0.2
+
 # Files and folders
 DATASET_FOLDER = "./dataset"
 EXPORT_FOLDER = "./export"
 DATASET_FILE = f"{EXPORT_FOLDER}/dataset.pkl"
+TRAIN_DATASET_FILE = f"{EXPORT_FOLDER}/train_dataset.pkl"
+VAL_DATASET_FILE = f"{EXPORT_FOLDER}/val_dataset.pkl"
+EXPANDED_DATASET_FILE = f"{EXPORT_FOLDER}/dataset_expanded.pkl"
+EXPANDED_TRAIN_DATASET_FILE = f"{EXPORT_FOLDER}/train_dataset_expanded.pkl"
+EXPANDED_VAL_DATASET_FILE = f"{EXPORT_FOLDER}/val_dataset_expanded.pkl"
--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
 from config import *
+from datetime import datetime
+import pandas as pd
 import pickle
 
 # Function to pickle an object
@@ -13,3 +15,14 @@ def load_pickle (filepath):
     pickled_object = pickle.load (file_pickle)
     file_pickle.close ()
     return pickled_object
+
+# Function to expand given dataframe for each sensor reading
+def expand_sensor_data(df, list_columns):
+    # Explode the DataFrame based on the list columns
+    df_new = pd.concat([df.drop(columns=list_columns), df[list_columns].apply(pd.Series.explode)], axis=1)
+    # Convert single element list to their values
+    df_new[list_columns] = df_new[list_columns].map(lambda x: x[0] if isinstance(x, list) and len(x) == 1 else x)
+    # Convert list of time values to a timestamp object
+    df_new['time'] = df_new['time'].apply(lambda x: datetime(year=int(x[0]), month=int(x[1]), day=int(x[2]), hour=int(x[3]), minute=int(x[4]), second=int(x[5]), microsecond=int((x[5] % 1) * 1e6)))
+    df_new.reset_index(inplace=True, drop=True)
+    return df_new
